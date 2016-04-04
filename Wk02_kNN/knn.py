7614974fc8abb83bc240bd092c4235c34d332c44
ringyoda <ringyoda@naver.com>
2016-04-04 23:55:52 +0900

    Last updated: 160404

--- Wk02_kNN/knn.py
+++
@@ -1,8 +0,0 @@
-
-from numpy import *
-import operator
-
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]]) # trainingSet
-    labels = ['A','A','B','B'] # labels
-    return group, labels
